19f0c028b91d7820a79168cef851d02ce431fef0
Roman Krutsko <romakrutsik@yahoo.com>
2026-04-07 18:50:43 +0300

    fix

--- task_equivalents/r/machine_learning_workflows/01_pipeline_cv_gridsearch_classification.R
+++ task_equivalents/r/machine_learning_workflows/01_pipeline_cv_gridsearch_classification.R
@@ -18,15 +18,15 @@ ctry = c(0.01, 0.1, 1, 10, 100, 1000)
 tune_ctrl = tune.control(cross=5)
 
 set.seed(1)
-tune.out1 = tune(svm, x=x, y=y, kernel='linear', ranges=list(cost=ctry), tunecontrol=tune_ctrl)
+tune.out1 = tune(svm, train.x=x, train.y=y, kernel='linear', ranges=list(cost=ctry), tunecontrol=tune_ctrl)
 summary(tune.out1)
 
 polytry = c(2, 3, 4, 5)
 set.seed(1)
-tune.out2 = tune(svm, x=x, y=y, kernel='polynomial', ranges=list(cost=ctry, degree=polytry), tunecontrol=tune_ctrl)
+tune.out2 = tune(svm, train.x=x, train.y=y, kernel='polynomial', ranges=list(cost=ctry, degree=polytry), tunecontrol=tune_ctrl)
 summary(tune.out2)
 
 gammatry = c(0.5, 1, 2, 3)
 set.seed(1)
-tune.out3 = tune(svm, x=x, y=y, kernel='radial', ranges=list(cost=ctry, gamma=gammatry), tunecontrol=tune_ctrl)
+tune.out3 = tune(svm, train.x=x, train.y=y, kernel='radial', ranges=list(cost=ctry, gamma=gammatry), tunecontrol=tune_ctrl)
 summary(tune.out3)
